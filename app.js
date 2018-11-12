0e660b9c7f95821c1c361808f87acfdfb8403f20
Kvoths <kvoths@gmail.com>
2018-11-12 20:18:46 +0100

    Añadiomos mongoose

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+//Modules
 var usersApp = require('./src/app/users/');
+
+//Routes
 app.use('/users', usersApp);
 app.get('/', (req, res) => res.send('Hello World!'))
 
